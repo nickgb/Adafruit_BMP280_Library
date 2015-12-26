db583d1fe2893af3f26ce9660b76fd70f2a070da
Nathan Heskew <heskew@gmail.com>
2015-12-26 11:58:37 -0800

    First real commit. Let's see how this goes.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Adafruit_BMP280_Library
-Particle Library for BMP280 sensors
+# Adafruit BMP280 driver for Particle devices
+
+Minimally modified version of the [Adafruit BMP280](http://www.adafruit.com/products/2651) [driver](https://github.com/adafruit/Adafruit_BMP280_Library) for [Particle](https://docs.particle.io/guide/getting-started) devices*. 
+
+**Only tested on the Photon so far*
+
+## About this driver
+I2C and SPI support is retained from the [original driver](). 
+
+### Wiring
+- BMP280: https://learn.adafruit.com/adafruit-bmp280-barometric-pressure-plus-temperature-sensor-breakout/wiring-and-test
+- Photon
+  - I2C: https://docs.particle.io/reference/firmware/photon/#wire-i2c-
+  - SPI: https://docs.particle.io/reference/firmware/photon/#spi
+
+## About Adafruit
+Adafruit invests time and resources providing this open source code. Please support Adafruit and open-source hardware by purchasing products from Adafruit!
---
+++ firmware/examples/BMP280.ino
@@ -0,0 +1,54 @@
+// IMPORTANT: When including a library in a firmware app, a sub dir prefix is needed
+// before the particular .h file.
+
+#include "Adafruit_BMP280_Library/Adafruit_Sensor.h"
+#include "Adafruit_BMP280_Library/Adafruit_BMP280.h"
+
+// #define BMP_CS A2
+// #define BMP_SCK A3
+// #define BMP_MISO A4
+// #define BMP_MOSI A5 
+
+Adafruit_BMP280 bmp; // I2C
+//Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
+//Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO, BMP_SCK);
+
+Timer timer(5000, checkEnvironment);
+
+// Update to reflect current pressure at sea level
+float seaLevelhPa = 1035.7;
+
+void setup() {
+
+  Particle.publish("DEBUG", "starting...");
+
+  if (bmp.begin()) {
+    Particle.publish("DEBUG", "starting the environment timer...");
+    timer.start();
+  }
+  else {
+    Particle.publish("WARN", "Could not find a valid BMP280 sensor, check wiring!");
+  }
+
+  Particle.publish("DEBUG", "started!");
+}
+
+void loop() {
+
+}
+
+void checkEnvironment() {
+
+  Particle.publish("DEBUG", "checking the environment...");
+  Particle.publish("environment/temperature", String(cToF(bmp.readTemperature())));
+  Particle.publish("environment/pressure", String(pToHg(bmp.readPressure())));
+  Particle.publish("environment/altitude", String(bmp.readAltitude(seaLevelhPa)));
+}
+
+float cToF(float c) {
+  return c * 9/5 + 32;
+}
+
+float pToHg(float p) {
+  return p/3389.39;
+}
